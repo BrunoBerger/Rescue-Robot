19a47a1847d421afdd55007b7451a54bc016a6dc
Lukas Walter <40722866+IxTzy@users.noreply.github.com>
2020-08-10 22:19:29 +0200

    code update

--- Implementierung/TheBot/RescueBot.cs
+++ Implementierung/TheBot/RescueBot.cs
@@ -18,13 +18,14 @@ namespace Implementierung
             this.startPoint = startP;
             this.endPoint = endP;
             Console.WriteLine("Motor {0} added!", this.motorName);
-            {
-                
-            }
         }
         public void getSignal()
         {
 
+        }
+        public string returnName()
+        {
+            return this.motorName;
         }
         public void motorOff()
         {
@@ -40,23 +41,51 @@ namespace Implementierung
     {   
         int positionX;
         int positionY;
-        object undergrund;
+        // Da bewegungen nur nach oben, unten, links und rechts möglich sind, sollte der Bot wissen was sich um ihn herum befindet und auf welchem untergrund er sich aktuell befindet.
+        object left;
+        object right;
+        object behind;
+        object inFront;
+        object current;
+        // Alle motoren innerhalöb des Bots Initialisieren
+        Motor motorChainDriveLeft;
+        Motor motorChainDriveRight;
+        Motor motorGrapplerRotation;
+        Motor motorGrapplerTilt;
+        Motor motorGrapplerJoint1;
+        Motor motorGrapplerJoint2;
+        Motor motorGripperRotation;
+        Motor motorGripper;
+        
 
         public RescueBot(int posX, int posY)
         {
             this.positionX = posX;
             this.positionY = posY;
             Console.WriteLine("Rescue Bot generated at X:{0}, Y:{1}", this.positionX,this.positionY);
+
+            // Alle motoren innerhalöb des Bots Initialisieren
+            this.motorChainDriveLeft = new Motor(100,"ChainDriveLeft",false,0,0);
+            this.motorChainDriveRight = new Motor(100,"ChainDriveRight",false,0,0);
+            this.motorGrapplerRotation = new Motor(100,"GrapplerRotation",false,0,360);
+            this.motorGrapplerTilt = new Motor(100,"GrapplerTilt",false,0,180);
+            this.motorGrapplerJoint1 = new Motor(100,"GrapplerJoint1",false,-90,90);
+            this.motorGrapplerJoint2 = new Motor(100,"GrapplerJoint2",false,-90,90);
+            this.motorGripperRotation = new Motor(100,"GripperRotation",false,0,360);
+            this.motorGripper = new Motor(100,"GrapplerTilt",false,0,100);
         }
 
-        
-        public void detectUnderground()
+        public void updateSurroundings(object Underground, object Left, object Right, object Behind, object InFront)
         {
-            
+            this.current = Underground;
+            this.left = Left;
+            this.right = Right;
+            this.behind = Behind;
+            this.inFront = InFront;
         }
         public void signalRequest()
         {
-            
+ 
         }
         public void getSignal()
         {
@@ -152,10 +181,10 @@ namespace Implementierung
             
             //Test
             Premises premises = new Premises(22, 20);
-
-
             premises.generateMap();
+            
 
+            
         }
     }
 }
