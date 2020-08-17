2b7fd588692c938c07343b9f4428890474eeb233
BrunoBerger <bruno.berger@stud.hshl.de>
2020-08-17 14:50:20 +0200

    added textures

--- Implementierung/Unity/Assets/scripts/gizmoHelper.cs
+++ Implementierung/Unity/Assets/scripts/gizmoHelper.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class gizmoHelper : MonoBehaviour
 {
-    // public float gizmoSize = .75f;
-    // public Color gizmoColor = Color.yellow;
-    //
-    // void OnDrawGizmos()
-    // {
-    //   Gizmos.color = gizmoColor;
-    //   Gizmos.DrawWireSphere(transform.position, gizmoSize);
-    // }
+    public float gizmoSize = .75f;
+    public Color gizmoColor = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+      Gizmos.color = gizmoColor;
+      Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
     void Update()
     {
       var currentHeading = transform.forward;
