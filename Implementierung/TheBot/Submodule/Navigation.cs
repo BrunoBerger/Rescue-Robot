a2f6babbd9f4b11447a613f4d57f5fd34d4add06
Lukas Walter <40722866+IxTzy@users.noreply.github.com>
2020-08-18 18:22:57 +0200

    Update Navigation.cs

--- Implementierung/TheBot/Submodule/Navigation.cs
+++ Implementierung/TheBot/Submodule/Navigation.cs
@@ -2,31 +2,35 @@ using System;
 
 namespace Implementierung
 {
-public class Navigation
-{
-    public Navigation()
+    public class Navigation
     {
-        Console.WriteLine("Navigation started!");
-    }
-    public void detectBarrier()
-    {
+        public Navigation()
+        {
+            Console.WriteLine("Navigation started!");
+        }
+        public void detectBarrier()
+        {
 
-    }
-    public void detectWater()
-    {
+        }
+        public void detectWater()
+        {
 
-    }
-    public void startNavigation(RescueBot rescuebot, int destX, int destY)
-    {
-        // navigation
-        // zu erst muss geguckt werden in welche richtung gefahren werden soll.
-        // dazu sollte ständig eine differnez zwischen x und y wert berechnet werden
-        // ziel ist es die differenz auf 1 1 zu bringen
-        // Sollte der bot vor einem Hinderniss stehen, muss er sich einen anderen Weg suchen
-        // ---> objekt umfahren und weiter zu ziel
-        
-    }
-}
+        }
+        public void startNavigation(RescueBot rescuebot, int destX, int destY)
+        {
+            // navigation
+            // zu erst muss geguckt werden in welche richtung gefahren werden soll.
+            // dazu sollte ständig eine differnez zwischen x und y wert berechnet werden
+            // ziel ist es die differenz auf 1 1 zu bringen
+            // Sollte der bot vor einem Hinderniss stehen, muss er sich einen anderen Weg suchen
+            // ---> objekt umfahren und weiter zu ziel
 
+            int destinationX = destX;
+            int destinationY = destY;
 
+            int differenzX = destinationX - rescuebot.returnXpos();
+            int differenzY = destinationY - rescuebot.returnYpos();
+
+        }
+    }
 }
