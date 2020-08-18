66fcc048fd35ead96377e97338334f8b890eba45
Lukas Walter <40722866+IxTzy@users.noreply.github.com>
2020-08-18 18:22:54 +0200

    Update Kommunikation.cs

--- Implementierung/TheBot/Submodule/Kommunikation.cs
+++ Implementierung/TheBot/Submodule/Kommunikation.cs
@@ -2,53 +2,51 @@ using System;
 
 namespace Implementierung
 {
-
-
-public class Peripherie
-{
-    
-    public void turnOn()
+    public class Peripherie
     {
+        
+        public void turnOn()
+        {
 
+        }
+        public void turnOff()
+        {
+
+        }
     }
-    public void turnOff()
+    public class Camera : Peripherie
     {
+        public Camera()
+        {
+            Console.WriteLine("Camera started!");
+        }
+        public void detectPerson()
+        {
 
+        }    
     }
-}
-public class Camera : Peripherie
-{
-    public Camera()
-    {
-        Console.WriteLine("Camera started!");
-    }
-    public void detectPerson()
-    {
 
-    }    
-}
-
-public class Microphon : Peripherie
-{
-    public Microphon()
+    public class Microphon : Peripherie
     {
-        Console.WriteLine("Microphon started!");
-    }
-    public void sendMessage()
-    {
+        public Microphon()
+        {
+            Console.WriteLine("Microphon started!");
+        }
+        public void sendMessage()
+        {
 
+        }
     }
-}
 
-public class Loudspeaker : Peripherie
-{
-    public Loudspeaker()
+    public class Loudspeaker : Peripherie
     {
-        Console.WriteLine("Loudspeaker started!");
-    }
-    public void sendMessage()
-    {
+        public Loudspeaker()
+        {
+            Console.WriteLine("Loudspeaker started!");
+        }
+        public void sendMessage()
+        {
 
+        }
     }
-}
 }
